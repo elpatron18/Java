77c99c2500ccc93b33f4fd9b23a12ad0136b6103
elpatron18 <timoantoniak18@gmail.com>
2023-01-17 15:20:50 +0100

    Fuhrpark hinzugefügt

---
+++ src/PR1/PR1_Aufgaben/A004_Fuhrpark/Auto.java
@@ -0,0 +1,14 @@
+package PR1.PR1_Aufgaben.A004_Fuhrpark;
+
+public class Auto {
+
+    private String marke;
+    private int ps;
+    private String typ;
+
+
+    public static void main(String[] args){
+
+    }
+
+}
