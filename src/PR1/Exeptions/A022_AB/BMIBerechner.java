0a64bcba9420efb4baee0f08a10f0681225a4c6e
el patron <60261111+elpatron18@users.noreply.github.com>
2023-03-23 08:56:39 +0100

    BMIBerechner Updated

--- src/PR1/Exeptions/A022_AB/BMIBerechner.java
+++ src/PR1/Exeptions/A022_AB/BMIBerechner.java
@@ -2,9 +2,9 @@ package PR1.Exeptions.A022_AB;
 
 public class BMIBerechner {
 
-    static double berechneBMI(double gewicht, double groesse) {
-        if (groesse <= 0 || groesse > 3) throw new ArithmeticException("Die Körpergröße ist nicht möglich");
-        if (gewicht <= 0 || gewicht > 500) throw new ArithmeticException("Das Gewicht ist nicht möglich");
+    static double berechneBMI(double gewicht, double groesse) throws Exception {
+        if (groesse <= 0 || groesse > 3) throw new Exception("Die Körpergröße ist nicht möglich");
+        if (gewicht <= 0 || gewicht > 500) throw new Exception("Das Gewicht ist nicht möglich");
 
         return gewicht / (groesse * groesse);
     }
