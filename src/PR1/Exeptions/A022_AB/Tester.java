dbd4b6c4b10919d4d64cd5d0e3b620b81190dedf
el patron <60261111+elpatron18@users.noreply.github.com>
2023-03-23 08:40:50 +0100

    Tester Updated

--- src/PR1/Exeptions/A022_AB/Tester.java
+++ src/PR1/Exeptions/A022_AB/Tester.java
@@ -3,9 +3,11 @@ package PR1.Exeptions.A022_AB;
 public class Tester {
     public static void main(String[] args) {
         try {
-            BMIBerechner.berechneBMI(83, 193);
-            Quersumme.berechneQuersumme(123);
-            Wurzelberechner.berechneWurzel(20);
+            System.out.println(
+                    BMIBerechner.berechneBMI(83, 1.93) + "\n" +
+                    Quersumme.berechneQuersumme(123) + "\n" +
+                    Wurzelberechner.berechneWurzel(20) + "\n"
+            );
 
             for (double d : MNFBerechner.berechneMNF(1, 0, 0)) System.out.println(d);
 
