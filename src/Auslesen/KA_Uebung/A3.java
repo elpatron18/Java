afbaa4db5a959ce41bd8d28f1d0ebb7a3a47906c
el patron <60261111+elpatron18@users.noreply.github.com>
2023-06-13 20:50:47 +0200

    push

--- src/Auslesen/KA_Uebung/A3.java
+++ src/Auslesen/KA_Uebung/A3.java
@@ -1,19 +1,60 @@
 package Auslesen.KA_Uebung;
 
-import java.io.FileReader;
+import javax.swing.*;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 
 public class A3 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        ArrayList<String> text = new ArrayList<String>();
 
         String speicherort = "C:\\Users\\Timo\\IdeaProjects\\Java\\src\\Auslesen\\KA_Uebung\\startDatei.txt";
+        String outputOrt = "C:\\Users\\Timo\\IdeaProjects\\Java\\src\\Auslesen\\KA_Uebung\\outputDatei.txt";
 
-        FileReader fr = null;
+        String line;
+
+        Charset charset = Charset.forName("UTF-8");
+
+        BufferedReader reader = null;
+        BufferedWriter bw = null;
+
+        JFrame fenster = new JFrame();
+        fenster.setSize(400, 250);
+        fenster.setVisible(true);
+
+        JTextField quelldatei = new JTextField();
+        //quelldatei.setText("Dateiname der Quelldatei");
+        fenster.add(quelldatei);
+
+        JButton button = new JButton();
+        button.setText("Submit");
+
+        fenster.add(button);
 
         try {
-            fr = new FileReader(speicherort);
+
+            reader = Files.newBufferedReader(Path.of(speicherort));
+            bw = Files.newBufferedWriter(Path.of(outputOrt), charset, StandardOpenOption.CREATE);
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                bw.write(line);
+                bw.newLine();
+            }
+            bw.close();
 
         } catch (Exception e) {
+            System.out.println("Fehler:" + e);
+        }finally {
+            if (reader != null) {
+                reader.close();
+            }
 
         }
 
